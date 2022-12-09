f6acbf6597dccad15b66d18a4705c0f1e2bcbffe
mohd <mohammad2java@gmail.com>
2022-12-09 21:55:32 +0530

    initial test

---
+++ webtest2/src/main/java/com/amir/Webtest2Application.java
@@ -0,0 +1,13 @@
+package com.amir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtest2Application.class, args);
+	}
+
+}
